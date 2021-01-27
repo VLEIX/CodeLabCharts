b776b5294a2ecd562291148d92d930dfcba09173
VLEIX <vleix.92@gmail.com>
2021-01-26 20:46:20 -0500

    first commit

---
+++ app/src/main/java/com/frantun/codelabcharts/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.frantun.codelabcharts
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+val parties = arrayOf(
+        "Party A", "Party B", "Party C"
+)
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setData()
+    }
+
+    private fun setData() {
+        val entries = ArrayList<PieEntry>()
+
+        parties.forEach {
+            entries.add(PieEntry(10F,
+                    it,
+                    ContextCompat.getDrawable(this, R.drawable.ic_android_black_24dp)))
+        }
+
+        val dataSet = PieDataSet(entries, "Election Results")
+        dataSet.setDrawIcons(false)
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0F, 40F)
+        dataSet.selectionShift = 5f
+
+        // add a lot of colors
+        val colors = ArrayList<Int>()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.LIBERTY_COLORS) colors.add(c)
+        for (c in ColorTemplate.PASTEL_COLORS) colors.add(c)
+        colors.add(ColorTemplate.getHoloBlue())
+        dataSet.colors = colors
+        //dataSet.setSelectionShift(0f);
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.WHITE)
+//        data.setValueTypeface(tfLight)
+
+        // set component pieChart
+        pie_chart.data = data
+
+        // undo all highlights
+        pie_chart.highlightValues(null)
+        pie_chart.invalidate()
+    }
+}
